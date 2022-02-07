4fab1d4538b291be5ad3305cef711cddec9104f7
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-07 13:51:36 +0100

    Encyclopedia definition created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Hello } from "./components/Hello";
+import { Term } from "./components/Term";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Hello />
+      <Term
+        name="Antidisestablishmentarianism"
+        description="A political position that developed in 19th-century Britain in opposition to Liberal proposals for the disestablishment of the Church of England—meaning the removal of the Anglican Church's status as the state church of England, Ireland, and Wales. The establishment was maintained in England, but in Ireland, the Church of Ireland (Anglican) was disestablished in 1871. In Wales, four Church of England dioceses were disestablished in 1920 and became the Church in Wales."
+      />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/Term.js
@@ -0,0 +1,8 @@
+export const Term = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </>
+  );
+};
